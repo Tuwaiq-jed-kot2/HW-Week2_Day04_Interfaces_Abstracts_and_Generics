bf11150b73bf11f18ebae6d127306121b4724c64
roya allam <Roya.Adel.jed.k2@tuwaiq.edu.sa>
2021-10-21 03:29:45 +0300

    thank you

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,10 +1,29 @@
-class Genter<G>(gen: G){
-    fun subtime(item:G){
+class Genterstring<G>(gen: G){
+    fun numSe(item:G){
         println(item)
     }
 }
+class Genternumber <A>(gen: A){
+    fun nnm(itemName:A){
+        println(itemName)
+    }
+}
+class GenterDou <D>(gen: D){
+    fun numD(itemDou:D){
+        println(itemDou)
+    }
+}
+
+
+
+
+
+
 
 fun main() {
-    val ggg=Genter("number")
-    ggg.subtime("one,two,three,four,five.six")
-}
+    val SSS = Genterstring("string ")
+    SSS.numSe("one,two,three,four,five.six")
+    val aaa=Genternumber(1)
+    aaa.nnm(515)
+    val DDD=GenterDou(10.0)
+    DDD.numD(20.0)}
