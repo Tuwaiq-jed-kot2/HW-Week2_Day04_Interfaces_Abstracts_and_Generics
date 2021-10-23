3f9dd751ec7ecf430eed66faad7bbe979139cf7b
AsrarAlbishri <Asrar.Mohammad.jed.k2@tuwaiq.edu.sa>
2021-10-24 00:27:11 +0300

    done:)

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,5 @@
+
+//part2 - Q1
 class dataTypes <A> (elements: A) {
 
     init {
@@ -14,4 +16,17 @@ fun main(){
     var data4 = dataTypes(25.5f)
     var data5 = dataTypes("Welcom")
 }
+
+//part 2 - Q2
+
+//Create three Generic Classes examples, one with Where, one with Out keyword
+// and the third with In keyword
+
+/*the out keyword on the generic type. It means that we can assign
+this reference to any of its supertypes. The out value can be only be
+ produced by the given class but not consumed*/
+
+/*the in keyword on the generic type if we want to assign it to the reference
+of its subtype. The in keyword can be used only on the parameter type
+ that is consumed, not produced */
 
